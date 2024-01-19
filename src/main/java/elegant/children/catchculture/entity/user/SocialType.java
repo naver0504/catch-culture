bf27f0a537eb98ea1135e980e8186f27025ea4b9
naver0504 <qortndwls1234@gmail.com>
2024-01-19 10:44:14 +0900

    feat: SocialType으로 OAuth2UserInfo 생성 로직 이동

--- src/main/java/elegant/children/catchculture/entity/user/SocialType.java
+++ src/main/java/elegant/children/catchculture/entity/user/SocialType.java
@@ -1,13 +1,34 @@
 package elegant.children.catchculture.entity.user;
 
+import elegant.children.catchculture.common.security.oauth2.userinfo.GoogleOAuth2UserInfo;
+import elegant.children.catchculture.common.security.oauth2.userinfo.KakaoOAuth2UserInfo;
+import elegant.children.catchculture.common.security.oauth2.userinfo.NaverOAuth2UserInfo;
+import elegant.children.catchculture.common.security.oauth2.userinfo.OAuth2UserInfo;
+
 import java.util.Arrays;
+import java.util.Map;
 
 
 public enum SocialType {
 
-    KAKAO,
-    NAVER,
-    GOOGLE;
+    KAKAO{
+            public OAuth2UserInfo toOAuth2UserInfo(Map<String, Object> attributes) {
+                return new KakaoOAuth2UserInfo(attributes);
+            }
+    },
+    NAVER{
+            public OAuth2UserInfo toOAuth2UserInfo(Map<String, Object> attributes) {
+                return new NaverOAuth2UserInfo(attributes);
+            }
+    },
+    GOOGLE {
+        public OAuth2UserInfo toOAuth2UserInfo(Map < String, Object > attributes){
+            return new GoogleOAuth2UserInfo(attributes);
+        }
+    };
+
+
+    public abstract OAuth2UserInfo toOAuth2UserInfo(Map<String, Object> attributes);
 
     public static SocialType of(String socialType) {
         return Arrays.stream(SocialType.values())
